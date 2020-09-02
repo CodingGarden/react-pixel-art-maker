e69cd8f97a902320145dffee70c5f74d9f0d110d
Jacob McEwan <jacob@mchew.com>
2020-09-03 02:52:47 +1000

    Addded !rgb command compression.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,33 @@ import Grid from './components/Grid';
 import ColorPicker from './components/ColorPicker';
 import useStyles from './App.styles';
 
+//  A custom function to do a kinda-dict based compression.
+function smallify(command) {
+  let colours;
+  //  Allows for direct passing of a !rgb command or an array of colours.
+  if (typeof command === 'string') {
+    colours = command.split(/[ ,]/g);
+  } else colours = command;
+  if (colours[0].startsWith('!')) colours.shift();
+  const firsts = new Map();
+  let next = 0;
+  return colours
+    .reduce((res, colour) => {
+      //  Check if this colour has been specified. If it has use the index of the first occurance as it is garunteed to be shorter.
+      let outPart = '';
+      if (firsts.has(colour)) {
+        outPart += `*${firsts.get(colour).toString(16)}`;
+      } else {
+        //  Otherwise just ad the raw colour and save this as the first occurance.
+        outPart += colour;
+        firsts.set(colour, next);
+        next += 1;
+      }
+      return `${res}${outPart},`;
+    }, '!rgb ')
+    .slice(0, -1);
+}
+
 const offCell = {
   on: false,
   color: '#000000',
@@ -23,7 +50,7 @@ function App() {
     [cells]
   );
   const chatString = useMemo(
-    () => cells.map((cell) => cell.color.slice(1)).join(','),
+    () => smallify(cells.map((cell) => cell.color.slice(1))),
     [cells]
   );
   return (
@@ -40,12 +67,7 @@ function App() {
         ))}
       </div>
       <Grid cells={cells} setCells={setCells} currentColor={currentColor} />
-      <p className={classes.chatString}>
-        {/* eslint-disable-next-line */}
-        !rgb
-        {' '}
-        {chatString}
-      </p>
+      <p className={classes.chatString}>{chatString}</p>
     </div>
   );
 }
